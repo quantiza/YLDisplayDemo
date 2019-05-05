979eb366dc09531cb9f6d4f8d62ec1262c7342ac
yanglong <18205186136@163.com>
2019-05-05 20:57:58 +0800

    init

---
+++ YLDisplayDemo/YLDisplayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLDisplayDemo
+//
+//  Created by yangl on 2019/5/5.
+//  Copyright © 2019 yangl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
